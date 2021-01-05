69b6cf626d22801e4b6f8cbce933833a9274a8b0
DJMong <ldj9604@gmail.com>
2021-01-05 12:05:04 +0900

    heap structure from array

--- heap.cpp
+++ heap.cpp
@@ -1,64 +1,37 @@
 #include <iostream>
 
 using namespace std;
-typedef int DATA;
 
-typedef struct heap
-{
-    DATA val;
-    heap *left;
-    heap *right;
-} heap;
-
-bool is_empty(heap *node)
-{
-    if(node == NULL)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-    return false;
-}
-
-DATA heap_pop(heap *root, DATA val)
-{
-    //search
-    //delete
-    //sort
-    //return
-}
-
-void heap_push(heap *root, DATA val)
-{
-    //search
-    //insert
-    //sort
-}
-
-static void _heap_del(heap *node)
-{
-    delete node;
-}
-
-static void _heap_add(heap **node, DATA val)
-{
-    node = new heap();
-    node->val = val;
-    cout << *node->val << endl;
-    node->left = NULL;
-    node->right = NULL;
-}
+void go_left(int &idx);
+void go_right(int &idx);
 
 int main()
 {
-
-    heap *root;
-    DATA val = 1;
-    _heap_add(&root, val);
-    cout << root->val << endl;
+    int arr[256];
+    int idx = 0;
+    for(int i = 0; i < 256; i++)
+    {
+        arr[i] = i;
+    }
+    cout << "now : " << arr[idx] << endl;
+    go_right(idx);
+    cout << "left : " << arr[idx] << endl;
+    go_left(idx);
+    cout << "right : " << arr[idx] << endl;
+    go_right(idx);
+    cout << "left : " << arr[idx] << endl;
     cout << "compile complete" << endl;
     return 0;
+}
+
+void go_left(int &idx)
+{
+    idx <<= 1;
+    idx += 1;
+}
+
+void go_right(int &idx)
+{
+    idx <<= 1;
+    idx += 2;
 }
